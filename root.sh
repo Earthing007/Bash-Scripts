4b61fdb3f66133bf3e52a3a3f3a0ef281ad9f967
Earthing007 <80304755+Earthing007@users.noreply.github.com>
2021-03-19 14:14:40 +0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,45 +1,44 @@
 #!/bin/bash
 
-# Verify root
+rd=$'\033[31m'; # Red
+gr=$'\033[32m'; # Green
+or=$'\033[33m'; # Orange
+nc=$'\033[0m'; # No Color
+
+# Check if root then exit if not
 if [ "$(whoami)" != 'root' ]; then
-	echo -e " \e[33m This script must be run as root\e[0m"
+	echo "${rd}This script must be run as root${nc}"
 	exit
 fi
 
-# Permit root login
 clear
-sleep 1
 if [[ $(sudo sshd -T | grep -i "permitrootlogin" | awk '{print $2}') = 'yes' ]]; then
 	echo -e "\n"
-	echo -e "Root Login already permitted."
+	echo "${or}PermitRootLogin already allowed.${nc}"
 else
 	echo -e "\n"
-	echo -e "\e[32mPermitting root login..\e[0m"
+	echo "${gr}Permitting root login..${nc}"
 	sed -i "s/PermitRootLogin no/PermitRootLogin yes/" /etc/ssh/sshd_config
 	sed -i "s/#PermitRootLogin.*/PermitRootLogin yes/" /etc/ssh/sshd_config
 fi
-
-# Allow password authentication
 sleep 1
 if [[ $(sudo sshd -T | grep -i "passwordauthentication" | awk '{print $2}') = 'yes' ]]; then
-	echo -e "PasswordAuthentication already allowed."
+	echo "${or}PasswordAuthentication already allowed.${nc}"
 else
-	echo -e "\e[32mAllowing password authentication..\e[0m"
+	echo "${gr}Allowing password authentication..${nc}"
 	sed -i "s/#PasswordAuthentication.*/PasswordAuthentication yes/" /etc/ssh/sshd_config
 	sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/" /etc/ssh/sshd_config
 fi
-
-# Restart sshd
 sleep 1
-echo -e "\e[32mRestarting sshd..\e[0m"
-systemctl restart sshd
 
-# Set root password
-echo -e "\n"
-echo -e "\e[32mNow enter password for root\e[0m\n"
-sleep 0.3
-passwd root
-echo -e "\n"
+echo "${or}Restarting sshd..${nc}"
+sudo systemctl restart sshd
 
-# Remove file
-rm -f root.sh
+echo -e "\n"
+echo "${gr}Now enter password for root${nc}"
+read -rp "< Default: i@mtheB3st >  --> " rootpass
+if [[ -z "$rootpass" ]]; then
+	rootpass="i@mtheB3st"
+fi
+echo "root:$rootpass" | chpasswd
+echo -e "\n"
